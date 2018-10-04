dab8961848007bdd12642be7062f410632bea637
aborisenok <lesha_borisenok@mail.ru>
2018-10-04 22:41:21 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,99 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.initialValue = `(${initialValue})`;
-    this.numberOfOperations = 0;
+    this.initialValue = `${initialValue}`;
   }
 
   add(number) {
-    this.initialValue += `+(${number})`;
-    this.numberOfOperations++;
+    this.initialValue += `+${number}`;
     return this;
   }
   
   subtract(number) {
-    this.initialValue += `-(${number})`;
-    this.numberOfOperations++;
+    this.initialValue += `-${number}`;
     return this;
   }
 
   multiply(number) {
-    this.initialValue += `*(${number})`;
-    this.numberOfOperations++;
+    this.initialValue += `*${number}`;
     return this;
   }
 
   devide(number) {
-    this.initialValue += `/(${number})`;
-    this.numberOfOperations++;
+    this.initialValue += `/${number}`;
     return this;
   }
 
   pow(number) {
-    this.initialValue += `^(${number})`;
-    this.numberOfOperations++;
+    this.initialValue += `**${number}`;
     return this;
   }
-  
-  calculate(str){
-  while(this.numberOfOperations-- > 0){
-    if(str.search(/\(\-*\d+\)\^\(\-*\d+\)/) != -1){
-      var expr;
-      if(str.search(/\^\(\-*\d+\)\^\(\-*\d+\)/) !=-1){
-        expr = str.match(/\^\(\-*\d+\)\^\(\-*\d+\)/g).pop();
-      }else{
-         expr = str.match(/\(\-*\d+\)\^\(\-*\d+\)/g)[0];
-      }
-      var values = expr.match(/\-*\d+/g);
-      if(expr[0] == "^"){
-        expr = expr.replace("^", "");
-      }
-      str = str.replace(expr, this.solve(+values[0], +values[1], "^"));
-    } else
-    if(str.search(/\(\-*\d+\)\*\(\-*\d+\)/) != -1){
-      var expr = str.match(/\(\-*\d+\)\*\(\-*\d+\)/)[0];
-      var values = expr.match(/\-*\d+/g);
-      str = str.replace(expr, this.solve(+values[0], +values[1], "*"));
-    } else
-    if(str.search(/\(\-*\d+\)\/\(\-*\d+\)/) != -1){
-      var expr = str.match(/\(\-*\d+\)\/\(\-*\d+\)/)[0];
-      var values = expr.match(/\-*\d+/g);
-      str = str.replace(expr, this.solve(+values[0], +values[1], "/"));
-    } else
-    if(str.search(/\(\-*\d+\)\-\(\-*\d+\)/) != -1){
-      var expr = str.match(/\(\-*\d+\)\-\(\-*\d+\)/)[0];
-      var values = expr.match(/\-*\d+/g);
-      str = str.replace(expr, this.solve(+values[0], +values[1], "-"));
-    }else
-    if(str.search(/\(\-*\d+\)\+\(\-*\d+\)/) != -1){
-      var expr = str.match(/\(\-*\d+\)\+\(\-*\d+\)/)[0];
-      var values = expr.match(/\-*\d+/g);
-      str = str.replace(expr, this.solve(+values[0], +values[1], "+"));
-    }
-  }  
-  return +str.match(/\-*\d+/);
-  }
-  
-  solve(a, b, sign){
-  switch(sign) {
-  case "+":  
-    return `(${a + b})`;
-
-  case "-":  
-    return `(${a - b})`;
-      
-  case "*":  
-    return `(${a * b})`;
-      
-  case "/":  
-    return `(${a / b})`;
-   
-  case "^":  
-    return `(${Math.pow(a, b)})`;
-  }
-}
-
+ 
   toString(){
-    return this.calculate(this.initialValue);
+    return eval(this.initialValue);
   }
 }
 
